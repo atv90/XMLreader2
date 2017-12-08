44be5bef3e50da35ce8bfb639dc747200679b242
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-08 09:24:31 +0200

    Lisättiin esimerkki toimivasta XMLluku-koodista.

---
+++ XMLreader2/XMLreader2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLreader2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader(@"C:\Users\Anne\source\repos\XMLreader1\XMLtiedosto.xml");
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text:
+
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
